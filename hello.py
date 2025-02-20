bf5f23d6ad22bc5d019f9aaa5cca7923d0666a69
jibaresucm <jibares@ucm.es>
2025-02-20 20:17:33 +0100

    Completed hello and vagrant

--- hello.py
+++ hello.py
@@ -4,4 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<p>Hello, World</p>'
+    return '<p>Hello, World</p> <a href = "/about">Link to about</a>'
+
+@app.route('/about')
+def about():
+    return "<p>This is a Flask web app running in a Linux VM</p>"
